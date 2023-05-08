e9e7abd6ff4c3525b8c8c79809cb037204a5da79
nazalan <zalan2552@gmail.com>
2023-05-08 23:38:51 +0200

    szoba alakzatokkal

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -18,8 +18,8 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : 
-// Neptun : 
+// Nev    : Nagy Zalán
+// Neptun : V9T3UL
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
 // mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
@@ -35,6 +35,8 @@
 // Computer Graphics Sample Program: Ray-tracing-let
 //=============================================================================================
 #include "framework.h"
+
+
 
 struct Material {
 	vec3 ka, kd, ks;
@@ -91,12 +93,72 @@ struct Sphere : public Intersectable {
 	}
 };
 
+struct Sqare : public Intersectable {
+	vec3 a, b, c, d;
+	Sqare(const vec3& _r1, const vec3& _r2, const vec3& _r3, const vec3& _r4, Material* _material) {
+		a = _r1;
+		b = _r2;
+		c = _r3;
+		d = _r4;
+		material = _material;
+	}
+
+	Hit intersect(const Ray& ray) {
+		Hit hit;
+
+		vec3 n = cross((b - a), (c - a));
+		float t = dot((a - ray.start), n) / dot(ray.dir, n);
+		if (t < 0) return hit;
+		vec3 p = ray.start + ray.dir * t;
+
+		if (dot(cross(b - a, p - a), n) > 0
+			&& dot(cross(c - b, p - b), n) > 0
+			&& dot(cross(d - c, p - c), n) > 0
+			&& dot(cross(a - d, p - d), n) > 0
+			)
+		{
+			hit.t = t;
+			hit.position = ray.start + ray.dir * hit.t;
+			hit.normal = n;
+			hit.material = material;
+		}
+		return hit;
+	}
+};
+
+struct Triangle : public Intersectable {
+	vec3 r1, r2, r3;
+	Triangle(const vec3& _r1, const vec3& _r2, const vec3& _r3, Material* _material) {
+		r1 = _r1;
+		r2 = _r2;
+		r3 = _r3;
+		material = _material;
+	}
+	Hit intersect(const Ray& ray) {
+		Hit hit;
+		vec3 n = cross((r2 - r1), (r3 - r1));
+		float t = dot((r1 - ray.start), n) / dot(ray.dir, n);
+		if (t < 0) return hit;
+		vec3 p = ray.start + ray.dir * t;
+
+		if (dot(cross(r2 - r1, p - r1), n) > 0 && dot(cross(r3 - r2, p - r2), n) > 0 && dot(cross(r1 - r3, p - r3), n) > 0) {
+			hit.t = t;
+			hit.position = ray.start + ray.dir * hit.t;
+			hit.normal = n;
+			hit.material = material;
+		}
+		return hit;
+	}
+};
+
 class Camera {
 	vec3 eye, lookat, right, up;
+	float fov;
 public:
-	void set(vec3 _eye, vec3 _lookat, vec3 vup, float fov) {
+	void set(vec3 _eye, vec3 _lookat, vec3 vup, float _fov) {
 		eye = _eye;
 		lookat = _lookat;
+		fov = _fov;
 		vec3 w = eye - lookat;
 		float focus = length(w);
 		right = normalize(cross(vup, w)) * focus * tanf(fov / 2);
@@ -105,6 +167,9 @@ public:
 	Ray getRay(int X, int Y) {
 		vec3 dir = lookat + right * (2.0f * (X + 0.5f) / windowWidth - 1) + up * (2.0f * (Y + 0.5f) / windowHeight - 1) - eye;
 		return Ray(eye, dir);
+	}
+	vec3 getEye() {
+		return eye;
 	}
 };
 
@@ -125,21 +190,146 @@ class Scene {
 	std::vector<Intersectable*> objects;
 	std::vector<Light*> lights;
 	Camera camera;
-	vec3 La;
+	vec3 La = vec3(0, 0, 0);
 public:
 	void build() {
-		vec3 eye = vec3(0, 0, 2), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
+		vec3 eye = vec3(-2, 0, 3), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
+		//vec3 eye = vec3(0, 0, 3), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
 		float fov = 45 * M_PI / 180;
 		camera.set(eye, lookat, vup, fov);
 
-		La = vec3(0.4f, 0.4f, 0.4f);
-		vec3 lightDirection(1, 1, 1), Le(2, 2, 2);
-		lights.push_back(new Light(lightDirection, Le));
+		//vec3 lightDirection(10, 0, 0), Le(0.01, 0.01, 0.01);
+		//lights.push_back(new Light(lightDirection, Le));
+		// 
+		//szoba sarkai
+		vec3 a = vec3(-1, -1, 1);
+		vec3 b = vec3(1, -1, 1);
+		vec3 c = vec3(1, 1, 1);
+		vec3 d = vec3(-1, 1, 1);
+		vec3 e = vec3(-1, -1, -1);
+		vec3 f = vec3(1, -1, -1);
+		vec3 g = vec3(1, 1, -1);
+		vec3 h = vec3(-1, 1, -1);
 
-		vec3 kd(0.3f, 0.2f, 0.1f), ks(2, 2, 2);
+		vec3 kd(0.5f, 0.5f, 0.5f), ks(2, 2, 2);
 		Material* material = new Material(kd, ks, 50);
-		for (int i = 0; i < 500; i++)
-			objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), rnd() * 0.1f, material));
+		//négy fala a szobának, ami látszik
+		objects.push_back(new Sqare(e, f, g, h, material));
+		objects.push_back(new Sqare(b, f, g, c, material));
+		objects.push_back(new Sqare(d, c, g, h, material));
+		objects.push_back(new Sqare(a, b, f, e, material));
+
+		vec3 icosahedron[12] = {
+	vec3(0+1, -0.525731-1,  0.850651+1),
+	vec3(0.850651+1,  0-1,  0.525731+1),
+	vec3(0.850651+1,  0-1, -0.525731+1),
+	vec3(-0.850651+1,  0-1, -0.525731+1),
+	vec3(-0.850651+1,  0-1,  0.525731+1),
+	vec3(-0.525731+1,  0.850651-1,  0+1),
+	vec3(0.525731+1,  0.850651-1,  0+1),
+	vec3(0.525731+1, -0.850651-1,  0+1),
+	vec3(-0.525731+1, -0.850651-1,  0+1),
+	vec3(0+1, -0.525731-1, -0.850651+1),
+	vec3(0+1,  0.525731-1, -0.850651+1),
+	vec3(0+1,  0.525731-1,  0.850651+1)
+		};
+
+		int icosahedron_order[60] = {
+		2,  3,  7,
+		2,  8,  3,
+		4,  5,  6,
+		5,  4,  9,
+		7,  6,  12,
+		6,  7,  11,
+		10,  11,  3,
+		11,  10,  4,
+		8,  9,  10,
+		9,  8,  1,
+		12,  1,  2,
+		1,  12,  5,
+		7,  3,  11,
+		2,  7,  12,
+		4,  6,  11,
+		6,  5,  12,
+		3,  8,  10,
+		8,  2,  1,
+		4,  10,  9,
+		5,  9,  1
+		};
+
+		vec3 dodecahedron[20] = {
+		vec3(-0.57735-0.5, -0.57735-1, 0.57735-1),
+		vec3(0.934172- 0.5, 0.356822-1, 0-1),
+		vec3(0.934172- 0.5, -0.356822-1, 0-1),
+		vec3(-0.934172- 0.5, 0.356822-1, 0-1),
+		vec3(-0.934172- 0.5, -0.356822-1, 0-1),
+		vec3(0- 0.5, 0.934172-1, 0.356822-1),
+		vec3(0- 0.5, 0.934172-1, -0.356822-1),
+		vec3(0.356822- 0.5, 0-1, -0.934172-1),
+		vec3(-0.356822- 0.5, 0-1, -0.934172-1),
+		vec3(0- 0.5, -0.934172-1, -0.356822-1),
+		vec3(0- 0.5, -0.934172-1, 0.356822-1),
+		vec3(0.356822- 0.5, 0-1, 0.934172-1),
+		vec3(-0.356822- 0.5, 0-1, 0.934172-1),
+		vec3(0.57735- 0.5, 0.57735-1, -0.57735-1),
+		vec3(0.57735- 0.5, 0.57735-1, 0.57735-1),
+		vec3(-0.57735- 0.5, 0.57735-1, -0.57735-1),
+		vec3(-0.57735-0.5, 0.57735-1, 0.57735-1),
+		vec3(0.57735- 0.5, -0.57735-1, -0.57735-1),
+		vec3(0.57735- 0.5, -0.57735-1, 0.57735-1),
+		vec3(-0.57735- 0.5, -0.57735-1, -0.57735-1)
+		};
+
+		int dodecahedron_order[108] = {
+			19, 3, 2,
+			12, 19, 2,
+			15, 12, 2,
+			8, 14, 2,
+			18, 8, 2,
+			3, 18, 2,
+			20, 5, 4,
+			9, 20, 4,
+			16, 9, 4,
+			13, 17, 4,
+			1, 13, 4,
+			5, 1, 4,
+			7, 16, 4,
+			6, 7, 4,
+			17, 6, 4,
+			6, 15, 2,
+			7, 6, 2,
+			14, 7, 2,
+			10, 18, 3,
+			11, 10, 3,
+			19, 11, 3,
+			11, 1, 5,
+			10, 11, 5,
+			20, 10, 5,
+			20, 9, 8,
+			10, 20, 8,
+			18, 10, 8,
+			9, 16, 7,
+			8, 9, 7,
+			14, 8, 7,
+			12, 15, 6,
+			13, 12, 6,
+			17, 13, 6,
+			13, 1, 11,
+			12, 13, 11,
+			19, 12, 11
+		};
+
+		for (int i = 0; i < 20; i++) {
+			//icosahedron[i].x += 1;
+			//icosahedron[i].y -= 1;
+			//icosahedron[i].z += 1;
+			objects.push_back(new Triangle(icosahedron[icosahedron_order[3*i]-1]*0.5, icosahedron[icosahedron_order[3*i+1]-1]*0.5, icosahedron[icosahedron_order[3*i+2]-1]*0.5, material));
+		}
+
+		for (int i = 0; i < 36; i++) {
+			objects.push_back(new Triangle(dodecahedron[dodecahedron_order[3 * i] - 1] * 0.6, dodecahedron[dodecahedron_order[3 * i + 1] - 1] * 0.6, dodecahedron[dodecahedron_order[3 * i + 2] - 1] * 0.6, material));
+		}
+
 	}
 
 	void render(std::vector<vec4>& image) {
@@ -170,7 +360,9 @@ public:
 	vec3 trace(Ray ray, int depth = 0) {
 		Hit hit = firstIntersect(ray);
 		if (hit.t < 0) return La;
-		vec3 outRadiance = hit.material->ka * La;
+		float L = 0.2 * (1 + dot(-1*normalize(hit.normal), normalize(ray.dir)));
+		vec3 outRadiance = hit.material->ka* L;
+
 		for (Light* light : lights) {
 			Ray shadowRay(hit.position + hit.normal * epsilon, light->direction);
 			float cosTheta = dot(hit.normal, light->direction);
@@ -252,10 +444,7 @@ void onInitialization() {
 	scene.build();
 
 	std::vector<vec4> image(windowWidth * windowHeight);
-	long timeStart = glutGet(GLUT_ELAPSED_TIME);
 	scene.render(image);
-	long timeEnd = glutGet(GLUT_ELAPSED_TIME);
-	printf("Rendering time: %d milliseconds\n", (timeEnd - timeStart));
 
 	// copy image to GPU as a texture
 	fullScreenTexturedQuad = new FullScreenTexturedQuad(windowWidth, windowHeight, image);
